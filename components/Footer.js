6cb0eb3aeaaf95bbc8c569c4745f24bd890b4fb6
Jainesh Jain <jainesh940@gmail.com>
2021-08-05 01:10:00 +0530

    Day 1 of building an Airbnb clone.

---
+++ components/Footer.js
@@ -0,0 +1,48 @@
+function Footer() {
+    return (
+        <div className='bg-gray-200 grid grid-cols-1 sm:grid-cols-2
+        md:grid-cols-5 gap-y-10 space-x-2 px-32 py-14'>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold '>ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold '>COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>Jainesh's Fam</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold '>HOST</h5>
+                <p>Jainesh</p>
+                <p>Zero to Hero</p>
+                <p>Learning</p>
+                <p>Let's grow together</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold '>SUPPORT</h5>
+                <p>Help Centre</p>
+                <p>Trust & Safety</p>
+                <p>Say Hi Fam</p>
+                <p>Easter Eggs</p>
+                <p>For the Win</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold '>ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import {
+    GlobeAltIcon,
+    MenuIcon,
+    UserCircleIcon,
+    SearchIcon,
+    UsersIcon
+} from '@heroicons/react/solid'
+
+function Header() {
+    return (
+        <header
+            className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
+            {/* Left */}
+            <div
+                className='relative flex items-center h-10 cursor-pointer my-auto'>
+                <Image
+                    src='https://links.papareact.com/qd3'
+                    layout='fill'
+                    objectFit='contain'
+                    objectPosition='left' />
+            </div>
+            {/* Middle */}
+            <div
+                className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+                <input
+                    className='pl-5 text-sm text-gray-600 
+                placeholder-gray-400 flex-grow bg-transparent outline-none'
+                    type='text'
+                    palceholder='Start Your Search' />
+                <SearchIcon
+                    className='h-8 hidden md:inline-flex md:mx-2 
+                bg-red-400 text-white rounded-full p-2 cursor-pointer' />
+            </div>
+            {/* Right */}
+            <div className='flex items-center justify-end space-x-4'>
+                <p className='hidden md:flex-inline'>Become a host</p>
+                <GlobeAltIcon className='h-6 cursor-pointer'/>
+                <div className='flex items-center space-x-2 cursor-pointer border-2 p-2 rounded-full'>
+                    <MenuIcon className='h-6'/>
+                    <UserCircleIcon className='h-6'/>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
